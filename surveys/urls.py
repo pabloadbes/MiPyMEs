31642d993df7a02cddd59ce5fd3322d7623fadbd
pabloadbes <pabloadbes@gmail.com>
2024-06-15 18:34:47 -0300

    Finalizar formularios maquetado CRUD Empresas y Encuestas

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import SurveyListView, SurveyDetailView, SurveyCreate
+from .views import SurveyListView, SurveyDetailView, SurveyCreate, SurveyUpdate, SurveyDelete
 
 surveys_patterns = ([
     path('', SurveyListView.as_view(), name='surveys'),
     path('<int:pk>/<slug:slug>/', SurveyDetailView.as_view(), name='survey'),
-    path('create/', SurveyCreate.as_view(), name='create')
+    path('create/', SurveyCreate.as_view(), name='create'),
+    path('update/<int:pk>/', SurveyUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', SurveyDelete.as_view(), name='delete')
 ], 'surveys')
--- surveys/views.py
+++ surveys/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Survey
 
 # Create your views here.
@@ -14,3 +15,17 @@ class SurveyDetailView(DetailView):
 class SurveyCreate(CreateView):
     model = Survey
     fields = ['company']
+    success_url = reverse_lazy('surveys:surveys')
+    # debo redirigir a continuar llenando encuesta
+
+class SurveyUpdate(UpdateView):
+    model = Survey
+    fields = ['company']
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('surveys:update', args=[self.object.id])+'?ok'
+
+class SurveyDelete(DeleteView):
+    model = Survey
+    success_url = reverse_lazy('surveys:surveys')
