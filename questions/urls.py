9fbcd8f30bfde821016d21e7d33005788f729c01
pabloadbes <pabloadbes@gmail.com>
2024-06-17 01:13:28 -0300

    Trabajando en formularios questions

--- questions/urls.py
+++ questions/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import QuestionsListView, QuestionDetail
 
 urlpatterns = [
-    path('', views.questions, name='questions'),
-    path('<int:question_id>/<slug:question_slug>/', views.question, name='question'),
+    path('', QuestionsListView.as_view(), name='questions'),
+    path('<int:pk>/<slug:slug>/', QuestionDetail.as_view(), name='question'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,11 +1,28 @@
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Question
 
 # Create your views here.
-def questions(request):
-    questions = get_list_or_404(Question)
-    return render(request, 'questions/questions.html', {'questions':questions})
+# def questions(request):
+#     questions = get_list_or_404(Question)
+#     return render(request, 'questions/questions.html', {'questions':questions})
 
-def question(request, question_id, question_slug):
-    question = get_object_or_404(Question, id=question_id)
-    return render(request, 'questions/question.html', {'question':question})
+class QuestionsListView(ListView):
+    model = Question
+
+# def question(request, question_id, question_slug):
+#     question = get_object_or_404(Question, id=question_id)
+#     return render(request, 'questions/question.html', {'question':question})
+
+class QuestionDetail(DetailView):
+    model = Question
+
+
+
+# question types
+# 0:select one
+# 1:text
+# 2:number
+# 3:scale
+# 4:multiple select
