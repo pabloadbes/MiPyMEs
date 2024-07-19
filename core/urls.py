67a1346766a25b5e9094c48818af214f03d90bd5
pabloadbes <pabloadbes@gmail.com>
2024-07-19 00:39:55 -0300

    Trabajando en usuarios, autenticación y permisos

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePageView, SamplePageView
+from .views import HomePageView, RootRedirectView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name="home"),
-    path('sample/', SamplePageView.as_view(), name="sample"),
+    path('', RootRedirectView.as_view(), name="root"),
+    path('home/', HomePageView.as_view(), name="home"),
+    # path('', HomePageView.as_view(), name="home"),
+    # path('sample/', SamplePageView.as_view(), name="sample"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
+from django.urls import reverse
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
-from django.views.generic.detail import DetailView
+from django.views.generic.base import TemplateView, RedirectView
 
 class HomePageView(TemplateView):
     template_name = "core/home.html"
@@ -8,5 +8,13 @@ class HomePageView(TemplateView):
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, {'title':'MiPyMEs'})
 
-class SamplePageView(DetailView):
-    template_name = "core/sample.html"
+class HomeRedirectView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('home')
+   
+class RootRedirectView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return reverse('home')
+        else:
+            return reverse('login')
