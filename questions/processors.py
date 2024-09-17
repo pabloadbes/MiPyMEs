33ae9673342a5c851f2f946d3746309786f5286e
pabloadbes <pabloadbes@gmail.com>
2024-09-17 00:41:52 -0300

    Trabajando double select pregunta 55

--- questions/processors.py
+++ questions/processors.py
@@ -40,6 +40,7 @@ def ctx_questions(request):
    company_id = Survey.objects.get(id = survey_id).company_id
    company = Company.objects.get(id = company_id)
    template_type = "./question_detail_type_" + question.question_type.__str__() + ".html"
+   print(template_type)
    question_metadata = dict(question_type = question.question_type.__str__())
    items = []
    its = Item.objects.all().filter(question_id = question_id)
@@ -47,7 +48,6 @@ def ctx_questions(request):
       hide_item = False
       opts = Option.objects.all().filter(item_id = item)
       options = []
-      print(template_type)
       if question.question_type.__str__() == "double_select":
          print("ES DOUBLE SELECT")
          for option in opts:
@@ -60,17 +60,17 @@ def ctx_questions(request):
                   child_opts = Option.objects.all().filter(item_id = child_item.id)
                   for child_option in child_opts:
                      child_options.append(child_option)
-                     if child_option.id in question_metadata.values():
-                        question_metadata[option.id].append(child_option.id)
-                        print("question_metadata del double select")
-                        print(question_metadata[option.id])
-                     question_metadata[option.id] = child_option.id
-                     print("METADATA")
-                     print(question_metadata)
+                     #if child_option.id in question_metadata.values():
+                     question_metadata[option.id].append(child_option.id)
+                     print("question_metadata del double select")
+                     print(question_metadata[option.id])
+                     # question_metadata[option.id] = child_option.id
+                     # print("METADATA")
+                     # print(question_metadata)
                   child_items.append([child_item, child_options])
-               options.append([option, notes, child_items])
-            else:
-               options.append([option, notes])   
+               options.append([option, child_items])
+         items.append([item, options])
+            
       else:
          print("NO ES DOUBLE SELECT")
          for option in opts:
@@ -106,5 +106,5 @@ def ctx_questions(request):
    ctx['items'] = items
    ctx['template_type'] = template_type
    ctx['question_metadata'] = json_question_metadata
-
+   print(ctx)
    return ctx
--- questions/views.py
+++ questions/views.py
@@ -141,17 +141,18 @@ class QuestionDetail(TemplateView):
                                 children_options = Option.objects.all().filter(item_id = children_item.id)
                                 if len(children_options) == 1:
                                     children_option = children_options.first()
-                                    children_response = Response.objects.create(value = data[str(children_option.id)], option_id = children_option.id, survey_id = survey.id, created_by = user_id, updated_by = user_id)
-                                    response.save()
-                                    if Variable_List.objects.all().filter(option_id = children_option.id).exists():
-                                        vble = Variable_List.objects.get(option_id = children_option.id)
-                                        variable = Variable.objects.create(value = data[str(children_option.id)], survey_id = survey.id, variable_list_id = vble.id, created_by = user_id, updated_by = user_id)   
-                                        variable.save()
+                                    if data.get(str(children_option.id), False):
+                                        children_response = Response.objects.create(value = data[str(children_option.id)], option_id = children_option.id, survey_id = survey.id, created_by = user_id, updated_by = user_id)
+                                        response.save()
+                                        if Variable_List.objects.all().filter(option_id = children_option.id).exists():
+                                            vble = Variable_List.objects.get(option_id = children_option.id)
+                                            variable = Variable.objects.create(value = data[str(children_option.id)], survey_id = survey.id, variable_list_id = vble.id, created_by = user_id, updated_by = user_id)   
+                                            variable.save()
                                 else:
                                     for children_option in children_options:
                                         if Variable_List.objects.all().filter(option_id = children_option.id).exists():
                                             children_vble = Variable_List.objects.get(option_id = children_option.id)
-                                        if children_option.text == data[str(children_item.id)]:
+                                        if data.get(str(children_item.id), False) and children_option.text == data[str(children_item.id)]:
                                             children_response = Response.objects.create(value = True, option_id = children_option.id, survey_id = survey.id, created_by = user_id, updated_by = user_id)
                                             children_variable = Variable.objects.create(value = str(children_option.code), survey_id = survey.id, variable_list_id = children_vble.id, created_by = user_id, updated_by = user_id)   
                                             children_response.save()
